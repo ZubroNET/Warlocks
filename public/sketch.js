0296e3af00d57ead866a551545aa3ebe459cdc87
FruitSnack1 <tomas.ryzner@email.cz>
2017-12-26 23:12:39 +0100

    move interpolation

--- public/sketch.js
+++ public/sketch.js
@@ -1,63 +1,75 @@
 var socket;
-var anim = 0;
 var players = {};
 var Land = {};
-var bullets = [];
+var bullets = {};
 var img;
-function setup(){
-    createCanvas(windowWidth,windowHeight);
-    img = loadImage('img/lava.jpg');
-    for (var i = 0; i < width/128; i++) {
-      for (var j = 0; j < height/128; j++) {
-        image(img, i*128, j*128);
-      }
-    }
-    socket = io.connect('http://localhost:8080');
+var inter = false;
 
-    socket.on('move', function(data) {
-      players = data;
-    });
-    socket.on('land', function(land){
-      Land = land;
-    });
-    socket.on('bullets', function(data){
-      bullets = data;
-    });
-    socket.on('players', function(data){
-      players = data;
-    });
-    socket.on('disconnect', function(){
-      alert('Server is down');
-      window.location.replace("http://google.com");
-    });
-    textAlign(CENTER);
-    translate(width/2, height/2);
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  img = loadImage('img/lava.jpg');
+  for (var i = 0; i < width / 128; i++) {
+    for (var j = 0; j < height / 128; j++) {
+      image(img, i * 128, j * 128);
+    }
+  }
+  socket = io.connect('http://localhost:8080');
+
+  // socket.on('move', function(data) {
+  //   players = data;
+  // });
+  socket.on('land', function(land) {
+    Land = land;
+  });
+  socket.on('newPlayer', function(playerInfo) {
+    players[playerInfo.id] = new Player(0,0,playerInfo.id, playerInfo.col, playerInfo.name);
+  console.log(playerInfo.col);
+  });
+  socket.on('bullets', function(data) {
+    bullets = data;
+  });
+  socket.on('data', function(data) {
+    console.log(data);
+    players = data['players'];
+    bullets = data['bullets'];
+    Land = data['land'];
+  });
+  socket.on('players', function(data) {
+    for (var id in data) {
+      players[id].x = data[id].x;
+      players[id].y = data[id].y;
+      players[id].angle = data[id].angle;
+    }
+  });
+  socket.on('disconnect', function() {
+    alert('Server is down');
+    window.location.replace("http://google.com");
+  });
+  textAlign(CENTER);
+  translate(width / 2, height / 2);
 }
 
-function draw(){
-  for (var i = -width/2; i < width/2; i+=128) {
-    for (var j = -height/2; j < height/2; j+=128) {
+function draw() {
+  for (var i = -width / 2; i < width / 2; i += 128) {
+    for (var j = -height / 2; j < height / 2; j += 128) {
       image(img, i, j);
     }
   }
-  stroke(53,53,77);
+  stroke(53, 53, 77);
   strokeWeight(8);
   fill(100);
   ellipse(Land.x, Land.y, Land.d, Land.d);
-  for(var id in players){
+  for (var id in players) {
     showPlayer(players[id]);
   }
-  for (var i = 0; i < bullets.length; i++) {
-    showBullet(bullets[i]);
+  for (var id in bullets) {
+    showBullet(bullets[id]);
   }
   move();
 
-  if(anim != 0){
-    if(anim > 10) anim*=-1;
-    anim++;
-  }
-  if(players[socket.id] != null){
-    if(!players[socket.id].alive){
+
+  if (players[socket.id] != null) {
+    if (!players[socket.id].alive) {
       fill(255);
       textStyle(BOLD);
       textSize(200);
@@ -67,77 +79,86 @@ function draw(){
   }
 }
 
-function Player(){
-    this.x;
-    this.y;
+function Player() {
+  this.x;
+  this.y;
 }
 
 function move() {
-    var data = {
-        up:false,
-        down:false,
-        right:false,
-        left:false
-    };
-    if(keyIsDown(87)) data.up =true;
-    if(keyIsDown(83)) data.down =true;
-    if(keyIsDown(68)) data.right =true;
-    if(keyIsDown(65)) data.left =true;
-    socket.emit('move',data);
+  var data = {
+    up: false,
+    down: false,
+    right: false,
+    left: false
+  };
+  if (keyIsDown(87)) data.up = true;
+  if (keyIsDown(83)) data.down = true;
+  if (keyIsDown(68)) data.right = true;
+  if (keyIsDown(65)) data.left = true;
+  socket.emit('move', data);
 }
 
-function mouseMoved(){
-  if(players[socket.id] != null){
-    var dx = mouseX - width/2 - players[socket.id].x;
-    var dy = mouseY - height/2 - players[socket.id].y;
+function mouseMoved() {
+  if (players[socket.id] != null) {
+    var dx = mouseX - width / 2 - players[socket.id].x;
+    var dy = mouseY - height / 2 - players[socket.id].y;
     var angle = atan2(dy, dx);
     socket.emit('angle', angle);
   }
 }
 
-function setName(name){
-    socket.emit('name', name);
+function setName(name) {
+  socket.emit('name', name);
 }
 
-function submitName(){
+function submitName() {
   document.getElementById('defaultCanvas0').style.display = 'block';
   document.getElementsByClassName('container')[0].style.display = 'none';
   var name = document.getElementById('name').value;
   socket.emit('createPlayer', name);
 }
 
-function showPlayer(player){
-  if(!player.alive)
+function showPlayer(player) {
+  if (!player.alive)
     return;
+    player.px = lerp(player.px, player.x, 0.2);
+    player.py = lerp(player.py, player.y, 0.2);
+    if(inter){
+      var x = player.px;
+      var y = player.py;
+    }else{
+      var x = player.x;
+      var y = player.y;
+    }
   push();
 
   //ratation
-  translate(player.x, player.y);
+  translate(x, y);
   rotate(player.angle);
 
   //hands
   push();
   fill(color(player.col));
-  rotate(PI/2);
-  rotate(PI/4);
-  stroke(53,53,77);
+  rotate(PI / 2);
+  rotate(PI / 4);
+  stroke(53, 53, 77);
   strokeWeight(4);
-  ellipse(0, -player.r-5-abs(anim), 20, 20);
-  rotate(-PI/4*2);
-  ellipse(0, -player.r-5, 20, 20);
+  ellipse(0, -player.r - 5, 20, 20);
+  rotate(-PI / 4 * 2);
+  ellipse(0, -player.r - 5, 20, 20);
   pop();
 
   //body
   fill(color(player.col));
   strokeWeight(player.strokeWeight);
-  stroke(53,53,77);
-  ellipse(0, 0, player.r*2, player.r*2);
+  stroke(53, 53, 77);
+  ellipse(0, 0, player.r * 2, player.r * 2);
 
   //name
   push();
   rotate(-player.angle);
   fill(255);
-  stroke(53,53,77);
+  stroke(53, 53, 77);
   text(player.name, 0, 5);
   pop();
 
@@ -146,10 +167,10 @@ function showPlayer(player){
   rotate(-player.angle);
   fill(120);
   strokeWeight(3);
-  stroke(53,53,77);
-  rect(-player.r, player.r + 5, player.r*2, 8);
-  fill(0,255,0);
-  rect(-player.r, player.r + 5, map(player.hp, 0, 100, 0, player.r*2), 8);
+  stroke(53, 53, 77);
+  rect(-player.r, player.r + 5, player.r * 2, 8);
+  fill(0, 255, 0);
+  rect(-player.r, player.r + 5, map(player.hp, 0, 100, 0, player.r * 2), 8);
   pop();
 
   //overload
@@ -157,26 +178,43 @@ function showPlayer(player){
   rotate(-player.angle);
   fill(120);
   strokeWeight(3);
-  stroke(53,53,77);
-  rect(-player.r, player.r + 13, player.r*2, 8);
-  fill(0,0,255);
-  rect(-player.r, player.r + 13, map(player.overload, 0, 100, 0, player.r*2), 8);
+  stroke(53, 53, 77);
+  rect(-player.r, player.r + 13, player.r * 2, 8);
+  fill(0, 0, 255);
+  rect(-player.r, player.r + 13, map(player.overload, 0, 100, 0, player.r * 2), 8);
   pop();
 
   pop();
 }
 
-function showBullet(bullet){
+function showBullet(bullet) {
   strokeWeight(bullet.strokeWeight);
   fill(255);
-  ellipse(bullet.x, bullet.y, bullet.r*2, bullet.r*2);
+  ellipse(bullet.x, bullet.y, bullet.r * 2, bullet.r * 2);
 }
 
-function mousePressed(){
-  anim++;
-  console.log("MouseX: " + (mouseX-(width/2) - players[socket.id].x));
-  console.log("MouseY: " + (mouseY-(height/2) - players[socket.id].y));
-  var angle = atan2((mouseY)-(height/2) - players[socket.id].y, mouseX-(width/2) - players[socket.id].x  );
+function mousePressed() {
+  console.log("MouseX: " + (mouseX - (width / 2) - players[socket.id].x));
+  console.log("MouseY: " + (mouseY - (height / 2) - players[socket.id].y));
+  var angle = atan2((mouseY) - (height / 2) - players[socket.id].y, mouseX - (width / 2) - players[socket.id].x);
   console.log(angle);
-  socket.emit('shoot',angle);
+  socket.emit('shoot', angle);
+}
+
+function Player(x, y, id, col, name) {
+  this.x = x;
+  this.y = y;
+
+  this.px = 0;
+  this.py = 0;
+
+  this.r = 30;
+  this.id = id;
+  this.col = col;
+  this.name = name;
+  this.hp = 100;
+  this.angle;
+  this.overload = 100;
+  this.strokeWeight = 4;
+  this.alive = true;
 }
--- server.js
+++ server.js
@@ -1,140 +1,151 @@
 var players = {};
-var bullets = [];
+var bullets = {};
 var Land = {
-  x : 0,
-  y : 0,
-  d : 1000
+  x: 0,
+  y: 0,
+  d: 1000
 };
 
 //server setup
 var express = require('express');
 var app = express();
 var server = app.listen(8080, listen);
+
 function listen() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('listening at' + ':' + port);
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('listening at' + ':' + port);
 }
 app.use(express.static('public'));
 var io = require('socket.io')(server);
 
 //the rest
-io.sockets.on('connection', function (socket) {
-    console.log("New player ID: '" + socket.id + "'");
+io.sockets.on('connection', function(socket) {
+  console.log("New player ID: '" + socket.id + "'");
+  var data = {};
+  data['players'] = players;
+  data['bullets'] = bullets;
+  data['land'] = Land;
+  socket.emit('data', data);
 
-    socket.on('disconnect', function () {
-        console.log("Client has disconnected");
-        delete players[socket.id];
-    });
+  socket.on('disconnect', function() {
+    console.log("Client has disconnected");
+    delete players[socket.id];
+  });
 
-    socket.on('createPlayer', function(name){
-      players[socket.id] = new Player(0, 0, socket.id, getRandomColor(), name);
-      socket.emit('land', Land);
-      //io.clients[socket.id].send('test', socket.id);
-    });
+  socket.on('createPlayer', function(name) {
+    console.log("new player papi");
+    players[socket.id] = new Player(0, 0, socket.id, getRandomColor(), name);
+    var playerInfo = {
+        id : socket.id,
+        col: players[socket.id].col,
+        name : players[socket.id].name
+    }
+    io.sockets.emit('newPlayer', playerInfo);
+  });
 
-    socket.on('move', function (data) {
-        if (players[socket.id] != null) {
-          if(data.up) players[socket.id].y += -3;
-          if(data.down) players[socket.id].y += 3;
-          if(data.right) players[socket.id].x += 3;
-          if(data.left) players[socket.id].x += -3;
-          //io.sockets.emit('move', players);
-        }
-    });
+  socket.on('move', function(data) {
+    if (players[socket.id] != null) {
+      if (data.up) players[socket.id].y += -3;
+      if (data.down) players[socket.id].y += 3;
+      if (data.right) players[socket.id].x += 3;
+      if (data.left) players[socket.id].x += -3;
+      //io.sockets.emit('move', players);
+    }
+  });
 
-    socket.on('angle', function(angle){
-      if(players[socket.id] != null){
-        players[socket.id].angle = angle;
-        //io.sockets.emit('move', players);
+  socket.on('angle', function(angle) {
+    if (players[socket.id] != null) {
+      players[socket.id].angle = angle;
+      //io.sockets.emit('move', players);
+    }
+  });
+
+  socket.on('shoot', function(data) {
+    if (players[socket.id] != null) {
+      if (players[socket.id].overload == 100) {
+        //bullets.push(new Bullet(socket.id, data));
+        bullets[socket.id] = new Bullet(players[socket.id].angle, socket.id);
+        players[socket.id].overload = 0;
       }
-    });
-
-    socket.on('shoot', function(data){
-      if(players[socket.id] != null){
-        if(players[socket.id].overload == 100){
-          bullets.push(new Bullet(socket.id, data));
-          players[socket.id].overload = 0;
-        }
-      }
-    });
-}
-);
+    }
+  });
+});
 
 function Player(x, y, id, col, name) {
-    this.x = x;
-    this.y = y;
-    this.r = 30;
-    this.id = id;
-    this.col = col;
-    this.name = name;
-    this.hp = 100;
-    this.angle;
-    this.overload = 100;
-    this.strokeWeight = 4;
-    this.alive = true;
+  this.x = x;
+  this.y = y;
+  this.r = 30;
+  this.id = id;
+  this.col = col;
+  this.name = name;
+  this.hp = 100;
+  this.angle;
+  this.overload = 100;
+  this.strokeWeight = 4;
+  this.alive = true;
 
-    this.hit = 0;
-    this.bulletAngle;
+  this.hit = 0;
+  this.bulletAngle;
 
-    this.update = function(){
-      var x = Math.abs(this.x);
-      var y = Math.abs(this.y);
-      var d = Math.sqrt(x*x + y*y);
-      if(d > Land.d/2-this.r+4 && this.hp > 0){
-        this.hp--;
-      }
-      if(this.overload <100){
-        this.overload+=0.5;
-      }
-      if(this.hit){
-        this.onHit();
-      }
-      if(this.hp == 0){
-        this.alive = false;
-      }
+  this.update = function() {
+    var x = Math.abs(this.x);
+    var y = Math.abs(this.y);
+    var d = Math.sqrt(x * x + y * y);
+    if (d > Land.d / 2 - this.r + 4 && this.hp > 0) {
+      this.hp--;
     }
-
-    this.onHit = function(){
-      var dX = Math.cos(this.bulletAngle);
-      var dY = Math.sin(this.bulletAngle);
-      this.x = this.x + 5 * dX;
-      this.y = this.y + 5 * dY;
-      this.hit--;
+    if (this.overload < 100) {
+      this.overload += 0.5;
     }
+    if (this.hit) {
+      this.onHit();
+    }
+    if (this.hp == 0) {
+      this.alive = false;
+    }
+  }
+
+  this.onHit = function() {
+    var dX = Math.cos(this.bulletAngle);
+    var dY = Math.sin(this.bulletAngle);
+    this.x = this.x + 5 * dX;
+    this.y = this.y + 5 * dY;
+    this.hit--;
+  }
 }
 
-function Bullet(id, angle){
+function Bullet(angle, id) {
   this.x = players[id].x;
   this.y = players[id].y;
   this.r = 8;
   this.angle = angle;
   this.speed = 15;
-  this.playerId = id;
   this.strokeWeight = 2;
+  this.playerId = id;
 
-  this.update = function(){
+  this.update = function() {
     var dX = Math.cos(this.angle);
     var dY = Math.sin(this.angle);
     this.x = this.x + this.speed * dX;
     this.y = this.y + this.speed * dY;
   }
 
-  this.collides = function(){
-    for(var id in players)
-    if(this. playerId != id){
-      var a = Math.abs(this.x - players[id].x);
-      var b = Math.abs(this.y - players[id].y);
-      var d = Math.sqrt(a*a + b*b);
-      if(d < players[id].r + players[id].strokeWeight + this.strokeWeight){
-        players[id].hit = 40;
-        players[id].bulletAngle = this.angle;
-        return true;
-      }else{
-        return false;
+  this.collides = function() {
+    for (var id in players)
+      if (this.playerId != id) {
+        var a = Math.abs(this.x - players[id].x);
+        var b = Math.abs(this.y - players[id].y);
+        var d = Math.sqrt(a * a + b * b);
+        if (d < players[id].r + players[id].strokeWeight + this.strokeWeight) {
+          players[id].hit = 40;
+          players[id].bulletAngle = this.angle;
+          return true;
+        } else {
+          return false;
+        }
       }
-    }
-    if(this.x > 2000 || this.x < -2000 || this.y > 2000 || this.y < -2000){
+    if (this.x > 2000 || this.x < -2000 || this.y > 2000 || this.y < -2000) {
       return true;
     }
   }
@@ -142,30 +153,40 @@ function Bullet(id, angle){
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 //main loop
-setInterval(function(){
-  if(Land.d >0){
-    Land.d-=0.1;
+setInterval(function() {
+  if (Land.d > 0) {
+    Land.d -= 0.1;
   }
   io.sockets.emit('land', Land);
 
-  for(var id in players){
+  for (var id in players) {
     players[id].update();
   }
-  for (var i = 0; i < bullets.length; i++) {
-    bullets[i].update();
-    if(bullets[i].collides()){
-      bullets.splice(i);
+  for (var id in bullets) {
+    bullets[id].update();
+    if (bullets[id].collides()) {
+      delete bullets[id];
     }
   }
   io.sockets.emit('bullets', bullets);
-  io.sockets.emit('players', players);
-}, 1000/50);
+  //io.sockets.emit('players', players);
+  var o = {};
+  for (var id in players) {
+    o[id] = {
+      x: players[id].x,
+      y: players[id].y,
+      angle: players[id].angle
+    }
+  }
+  io.sockets.emit('players', o);
+
+}, 1000 / 15);
